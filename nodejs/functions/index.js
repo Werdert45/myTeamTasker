f92f2d5f69d5aeb628e7bd909b9bbf2a3df2e7a8
Ian Ronk <ian.ronk0@gmail.com>
2020-07-16 11:46:10 +0200

    APP-21: function works correctly

--- nodejs/functions/index.js
+++ nodejs/functions/index.js
@@ -34,17 +34,18 @@ exports.removeOldSingleTask = functions.pubsub.schedule('0 3 * * *')
         var n = d.getTime();
 
         const reference = database.collection('single_tasks/');
-        const snapshot = await reference.where('date', '<', n)
-//            .where('finished', '==', true)
-            .get();
+        const snapshot = await reference.where('date', '<', n).get();
         if (snapshot.empty) {
             console.log('no matching documents');
             return ;
         }
 
         snapshot.forEach(doc => {
-            if (doc.finished) {
+            console.log(doc);
+            if (doc.data().finished) {
                 database.doc('single_tasks/' + doc.id).delete();
+                // also remove entry in array of single_personal_tasks or group_tasks
+                // (or create clause when searching, if not found, remove entry): !!!!!
             }
         });
 
